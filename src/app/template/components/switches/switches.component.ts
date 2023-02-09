f761eb618578253b6a86dc77f7fb68de69ad8d61
JuanRamirez <juanramirez8034@gmail.com>
2023-02-09 15:05:49 -0400

    Terminada la seccion de switches forms por template, modificado el template del alertComponent

--- src/app/template/components/switches/switches.component.ts
+++ src/app/template/components/switches/switches.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlerConfig } from 'src/app/components/interfaces/interfaces';
+
+interface Person{
+  gender:string;
+  notifications:boolean;
+}
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
+  // objeto alert para controlar el alerta
+  public alertConfig : AlerConfig = {
+    showAlert:false,
+    showSpinner:false,
+    text:''
+  }
 
-  ngOnInit(): void {
+  // funcionalidad para el alert
+  public hideAlert(time_ms:number):void{
+    setTimeout(()=>{this.alertConfig = {showAlert:false, text:'', showSpinner:false};}, time_ms);
+  }
+
+  // objeto con las propiedades de la persona
+  public person : Person ={
+    gender : '',
+    notifications:true
+  }
+
+  public termsAndConditions : boolean = false;//variable enlazada a los terminos del formulario
+
+  public save(){
+    this.alertConfig = {//configuracion del alert a mostrar
+      showAlert:true, 
+      showSpinner:true, 
+      text:'Reseting the fields...', 
+      typeAlert:'success'
+    };
+
+    this.hideAlert(2000);//ocultando alerta luego de 2s
+
+    this.person.gender='';
+    this.person.notifications=true;
+    this.termsAndConditions =false;
   }
 
 }
